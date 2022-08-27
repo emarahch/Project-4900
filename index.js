35bb52efe3d3a1b701ff3be3f87307a102c4b181
emarahch <emarah.charles19@bcmail.cuny.edu>
2022-08-27 14:11:39 -0400

    woohooo

---
+++ index.js
@@ -0,0 +1,45 @@
+Parse.serverURL = 'https://parseapi.back4app.com'; // This is your Server URL
+// Remember to inform BOTH the Back4App Application ID AND the JavaScript KEY
+Parse.initialize(
+  'rspognStfNV37CQa4LHoJxZReWuNU1iXKgPyOVv2', // This is your Application ID
+  'ARHVg2q79aHYce2i4rov3RDm6Z4LMvPckfwggh6T' // This is your Javascript key
+);
+
+   
+
+        function create () {  //works
+        var user = new Parse.User();
+        user.set("username", document.getElementById("userName_field").value);
+        user.set("email", document.getElementById("email_field").value);
+        user.set("password", document.getElementById("password_field").value);
+       
+            // Call the save method, which returns the saved object if successful
+            user.signUp().then(function(user) {
+                window.alert('User created successful with name: ' + user.get("username") + ' and email: ' + user.get("email"));
+            }).catch(function(error){
+                window.alert("Error: " + error.code + " " + error.message);
+            });
+    
+    }
+
+    function login(){ ///struggling
+        var user = Parse.User
+        .logIn(document.getElementById("email_field").value,document.getElementById("password_field").value).then(function(user) {
+            window.alert("logged in babes");
+    }).catch(function(error){
+        window.alert("Error: " + error.code + " " + error.message);
+    });
+    };
+
+    function createAccountPage(){
+        location.href = "createAccount.html";
+    };
+
+  function loginPage(){
+    location.href = "loginPage.html";
+  };
+   
+
+
+       
+    
