7b3c3a1a7aa5b8d9ea679a51305638c617683b78
emarahch <emarah.charles19@bcmail.cuny.edu>
2022-10-24 12:14:54 -0400

    fixed issues

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ Parse.initialize(
   'ARHVg2q79aHYce2i4rov3RDm6Z4LMvPckfwggh6T' // This is your Javascript key
 );
 
+
+
+/// section 1 completed
+
+
 function mainProfilePage(){
   location.href = "ProfilePage.html";
+  
 };
 
 function login(){ ///struggling
@@ -31,9 +37,11 @@ location.href = "loginPage.html";
 function chooseYourCharacterPage(){
 location.href = "chooseYourChara.html";
 };
+/// section 1 completed
 
    
 
+// / section 2 completed
  function create () {  
    var user = new Parse.User();
    user.set("username", document.getElementById("userName_field").value);
@@ -49,159 +57,167 @@ location.href = "chooseYourChara.html";
     
     }
 
-    
-
+  
 
   function refUser(){
   Parse.User.enableUnsafeCurrentUser()
   const currentUser = Parse.User.current();
   refUse=currentUser.get("username");
    document.getElementById("welcome").innerHTML ="Hey! "+refUse;
-//  window.alert("Hey!"+" "+refUse);
+
+
+   //playong with local storage, this is here bc this is the onload functuon in the profile html
+  //  localStorage.setItem('myCat', 'Tom');
+  //  const cat = localStorage.getItem('myCat');
+  //  window.alert(cat)
+  // window.alert(toppp);
   }
+// section 2 completed
 
 
-//close but bc it starts at 0 the dunctionaly is off
- ob=0;
-  function storeTODO(){
-    (async () => {
-      const todo2= new Parse.Object('ToDo');
-      todo2.set('User', Parse.User.current());
-      todo2.set('title',document.querySelector('.todo-input').value);
-      todo2.set('isCompleted', false);
-      
-      try {
-        const result = await todo2.save();
-        console.log('ToDo created', result);
-      } catch (error) {
-        console.error('Error while creating ToDo: ', error);
-      }
-
-      ob=todo2.id
-      // window.alert(ob);
-    })();
-     }     
-
-     //still working on
-    // function deleteToDoStore(){
-    //   (async () => {
-    //     const query = new Parse.Query('ToDo');
-    //     try {
-    //       // here you put the objectId that you want to delete
-    //       const object = await query.get('xKue915KBG');
-    //       try {
-    //         const response = await object.destroy();
-    //         console.log('Deleted ParseObject', response);
-    //       } catch (error) {
-    //         console.error('Error while deleting ParseObject', error);
-    //       }
-    //     } catch (error) {
-    //       console.error('Error while retrieving ParseObject', error);
-    //     }
-    //   })();
-
-    // }
-
-//not perfect, but better. need to chnage it so you can complte task any time not just right after adding
-    function completedStore(idd){
-      (async () => {
-        const query = new Parse.Query('ToDo');
-        try {
-          // here you put the objectId that you want to update
-          const object = await query.get(idd);
-          object.set('isCompleted', true);
-          try {
-            const response = await object.save();
-            // You can use the "get" method to get the value of an attribute
-            // Ex: response.get("<ATTRIBUTE_NAME>")
-            // Access the Parse Object attributes using the .GET method
-            // console.log(response.get('myCustomKey1Name'));
-            // console.log('MyCustomClassName updated', response);
-          } catch (error) {
-            console.error('Error while updating ', error);
-          }
-        } catch (error) {
-          console.error('Error while retrieving object ', error);
-        }
-      })();
-    }
-
-
-
-  //the following is practice from a video to figure out code to potential 
 const todoInput= document.querySelector('.todo-input');
 const todoButton= document.querySelector('.todo-button');
 const todoList= document.querySelector('.todo-list');
- 
-todoButton.addEventListener("click", addTodo);
+
+todoButton.addEventListener("click", storeTODO);
 todoList.addEventListener("click", deleteCheck);
 
 
+function addTodo(idd){
+  // preventDefault();
+  const toDoDiv = document.createElement('div');
+  toDoDiv.setAttribute("id", idd);
+  toDoDiv.setAttribute("value", "OFF")  
+  toDoDiv.classList.add('todo');
 
-  function addTodo(event){
-    event.preventDefault();
-    storeTODO()
-    idd=ob;
-
-    const toDoDiv = document.createElement('div');
-    toDoDiv.setAttribute("id", idd);
-    toDoDiv.classList.add('todo');
-
-    const newToDo=document.createElement('li');
-    newToDo.innerText = todoInput.value;
-    newToDo.classList.add('todo-item');
-    toDoDiv.appendChild(newToDo);
+  const newToDo=document.createElement('li');
+  newToDo.innerText = todoInput.value;
+  newToDo.classList.add('todo-item');
+  toDoDiv.appendChild(newToDo);
 
 
-    const completedButton =document.createElement('button');
-    completedButton.innerText='done';
-    completedButton.classList.add('complete-btn');
-    toDoDiv.appendChild(completedButton);
+  const completedButton =document.createElement('button');
+  completedButton.innerText='done';
+  completedButton.classList.add('complete-btn');
+  toDoDiv.appendChild(completedButton);
 
-    const cancelButton =document.createElement('button');
-    cancelButton.innerText='trash';
-    cancelButton.classList.add("cancel-btn");
-    toDoDiv.appendChild(cancelButton);
+  const cancelButton =document.createElement('button');
+  cancelButton.innerText='trash';
+  cancelButton.classList.add("cancel-btn");
+  toDoDiv.appendChild(cancelButton);
 
 
 
-    //add to list
-    todoList.appendChild(toDoDiv);
+  //add to list
+  todoList.appendChild(toDoDiv);
+  
+  window.alert(toDoDiv.getAttribute("id"))// works
+  //clear inout value 
+
+  todoInput.value="";
+}
+
+//this stores new Todo to parse
+function storeTODO(event){
+  event.preventDefault();
+  (async () => {
+    const todo2= new Parse.Object('ToDo');
+    todo2.set('User', Parse.User.current());
+    todo2.set('title',document.querySelector('.todo-input').value);
+    todo2.set('isCompleted', false);
     
-    window.alert(toDoDiv.getAttribute("id"))// works
-    //clear inout value 
-    todoInput.value="";
+    try {
+      const result = await todo2.save();
+      console.log('ToDo created', result);
+    } catch (error) {
+      console.error('Error while creating ToDo: ', error);
+    }
 
-  }
-
-
-  //I might need to completlye redo the logic of the todo list
-
-function deleteCheck(event){
-  const item= event.target;
-  if(item.classList[0]=== "cancel-btn"){
-    const todo= item.parentElement;
-    todo.remove();
-  }
-
-  if(item.classList[0] === "complete-btn"){
-    const todo= item.parentElement;
-    todo.classList.toggle('completed');
-    completedStore(todo.getAttribute("id"));
-    // window.alert(todo.getAttribute("id"))
+    ob=todo2.id
+    addTodo(ob)
+  
 
    
+  })();
+  
+   }     
 
-    //idea, with each todo the id is stored, so here we can get the id of the parent element to dlete
-    // meow=todo.idd;
-    // window.alert(meow)
-    // completedStore(idd)
-    // completedStore(ob)
+   //this updates parse for task deletion
+  function deleteToDoStore(idd){
+    (async () => {
+      const query = new Parse.Query('ToDo');
+      try {
+        // here you put the objectId that you want to delete
+        const object = await query.get(idd);
+        try {
+          const response = await object.destroy();
+          console.log('Deleted ParseObject', response);
+        } catch (error) {
+          console.error('Error while deleting ParseObject', error);
+        }
+      } catch (error) {
+        console.error('Error while retrieving ParseObject', error);
+      }
+    })();
 
+  }
+
+
+  //this updates parse for task completion 
+  function completedStore(idd){
+    (async () => {
+      const query = new Parse.Query('ToDo');
+      try {
+        // here you put the objectId that you want to update
+        const object = await query.get(idd);
+        //this is so the user can toggle multiple times for the same task
+        meow=object.get('isCompleted');
+        if (meow==false){
+        object.set('isCompleted', true);
+        }
+        if (meow==true){
+          object.set('isCompleted', false);
+          }
+        try {
+          const response = await object.save();
+
+        } catch (error) {
+          console.error('Error while updating ', error);
+        }
+      } catch (error) {
+        console.error('Error while retrieving object ', error);
+      }
+      
+    })();
+  }
+
+
+
+// /currently working on
+
+function loginLoadUserTodo(idd){
+}
+//currently working on
+
+
+function deleteCheck(event){
+const item= event.target;
+if(item.classList[0]=== "cancel-btn"){
+  const todo= item.parentElement;
+  todo.remove();
+  deleteToDoStore(todo.getAttribute("id"))
+}
+
+
+
+//need to have it whhere the user can click button multuple times, bc people make mistakes - comeplted, but still do test
+if(item.classList[0] === "complete-btn"){
+  const todo= item.parentElement;
+  todo.classList.toggle('completed');
+  completedStore(todo.getAttribute("id"));
   }
 
 }
-
 
 
 
