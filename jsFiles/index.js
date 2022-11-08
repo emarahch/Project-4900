17485a0bf5e28b9b017681708824dbb7a0d83eb0
emarahch <emarah.charles19@bcmail.cuny.edu>
2022-11-08 15:24:58 -0500

    rearranging files

--- jsFiles/index.js
+++ jsFiles/index.js
@@ -1,14 +1,16 @@
-Parse.serverURL = 'https://parseapi.back4app.com'; // This is your Server URL
-// Remember to inform BOTH the Back4App Application ID AND the JavaScript KEY
-Parse.initialize(
-  'rspognStfNV37CQa4LHoJxZReWuNU1iXKgPyOVv2', // This is your Application ID
-  'ARHVg2q79aHYce2i4rov3RDm6Z4LMvPckfwggh6T' // This is your Javascript key
-);
-
-//If havinf trouble with parse, check if their is an update
+// Parse.serverURL = 'https://parseapi.back4app.com'; // This is your Server URL
+// // Remember to inform BOTH the Back4App Application ID AND the JavaScript KEY
+// Parse.initialize(
+//   'rspognStfNV37CQa4LHoJxZReWuNU1iXKgPyOVv2', // This is your Application ID
+//   'ARHVg2q79aHYce2i4rov3RDm6Z4LMvPckfwggh6T' // This is your Javascript key
+// );
 
 
-/// section 1 completed
+//REMOVE COMMENT WHEN TESTING
+
+//If having trouble with parse, check if there is an update
+
+
 
 
 
@@ -17,14 +19,10 @@ function mainProfilePage(){
   location.href = "../htmlFiles/ProfilePage.html";
   
 };
-
 //FOR BUTTONS END
 
 
-
-   
-
-// / section 2 completed
+//Creating an account
 
 function preCreate(){
   var charry="";
@@ -57,6 +55,7 @@ function create (charry) {
 }
 
 
+//logging into account
 function login(){
   var user = Parse.User
   .logIn(document.getElementById("email_field").value,document.getElementById("password_field").value).then(function(user) {
@@ -68,7 +67,7 @@ function login(){
 };
 
 
-
+//Body will call this upon loading the page
 function loader(){
   Parse.User.enableUnsafeCurrentUser()
   const currentUser = Parse.User.current();
@@ -82,7 +81,7 @@ function loader(){
 }
 
 
-
+//Adding New Todo's to the dom
 const todoInput= document.querySelector('.todo-input');
 const todoButton= document.querySelector('.todo-button');
 const todoList= document.querySelector('.todo-list');
@@ -272,5 +271,101 @@ function pasteToDo(results){
 
 
        
+//NOTES SECTION//
+
+const saveButtonNotes= document.querySelector('.btn-save');
+const clearButtonNotes= document.querySelector('.btn-clear');
+const notesInput= document.querySelector('.body-field');
+const notesList= document.querySelector('.notes-list');
+const notesTitle= document.querySelector('.title-field');
 
 
+saveButtonNotes.addEventListener("click", storeNote);
+clearButtonNotes.addEventListener("click", clearNote);
+notesList.addEventListener("click", deleteNote);
+
+
+
+
+function storeNote(event){
+  event.preventDefault();
+  addNote()
+//   window.alert("works");
+
+}
+
+  function clearNote(event){
+    event.preventDefault();
+    window.alert("works");
+  }
+
+
+  
+
+  function addNote(){
+    // preventDefault();
+    const notesDiv = document.createElement('div');
+    // toDoDiv.setAttribute("id", idd);
+    // toDoDiv.setAttribute("value", "OFF")  
+    notesDiv.classList.add('note');
+  
+
+    const noteTitle=document.createElement('li');
+    noteTitle.innerText= notesTitle.value;
+    noteTitle.classList.add('titleOut');
+    notesDiv.appendChild(noteTitle);
+
+    const newNote=document.createElement('li');
+    newNote.innerText = notesInput.value;
+    newNote.classList.add('note-item');
+    notesDiv.appendChild(newNote);
+
+
+    const timeCreated =document.createElement('li');
+    timeCreated.innerText=formatTime();
+    timeCreated.classList.add('timeCreatedOutput');
+    notesDiv.appendChild(timeCreated);
+  
+  
+    const cancelButton =document.createElement('button');
+    cancelButton.innerText='delete';
+    cancelButton.classList.add("cancel-btn");
+    notesDiv.appendChild(cancelButton);
+  
+  
+  
+    //add to list
+    notesList.appendChild(notesDiv);
+    
+    // window.alert(toDoDiv.getAttribute("id"))// workdeleteNote
+    //clear inout value 
+  
+    notesInput.value="";
+    notesTitle.value="";
+  }
+
+  
+  function deleteNote(event){
+    const item= event.target;
+    if(item.classList[0]=== "cancel-btn"){
+      const note= item.parentElement;
+      note.remove();
+    //   deleteToDoStore(todo.getAttribute("id"))
+    }
+}
+
+  // LOL CHANGE UP
+
+  //format time
+  function formatTime(str) {
+    // var parsed = new Date(str);
+    var parsed = new Date();
+    var date = zeroPad(parsed.getMonth() + 1) + "/"+ zeroPad(parsed.getDate())  + "/"+  parsed.getFullYear(); 
+    var time = zeroPad(parsed.getHours()) + ":" + zeroPad(parsed.getMinutes());
+    return ("Created: " + date + " | " + time);
+  }
+  //append 0's to hours/ minutes if single digits
+  function zeroPad(num) {
+    while(String(num).length < 2) { num = "0" + String(num); }
+    return num;
+  }
