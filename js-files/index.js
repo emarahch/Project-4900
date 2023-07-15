85e93dcf8648ae0eb988abb7d76c0bf838cc6fb8
emarahch <emarah.charles19@bcmail.cuny.edu>
2023-07-14 21:59:59 -0400

    music draggable

--- js-files/index.js
+++ js-files/index.js
@@ -255,6 +255,13 @@ changeDateHabitBtn.addEventListener("click", () => {
 });
 
 
+//showing hiding music
+const showMusic = document.getElementById("TriggerMusicButton");
+const divMusic= document.getElementById("music");
+showMusic.addEventListener("click", () => {
+  generalDisplayBlockCall(divMusic);
+});
+
 //Switching between archive view and normal view
 const viewArchiveButton = document.getElementById("viewArchiveButton");
 const CompletedTodoList = document.querySelector(".CompletedTodo-list");
@@ -1603,4 +1610,45 @@ function playerF(){
 function pauseF(){
     playButton.innerHTML = "▶";
     audio.pause();
+}
+
+
+
+// https://www.w3schools.com/howto/howto_js_draggable.asp
+dragElement(document.getElementById("music"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  elmnt.onmousedown = dragMouseDown;
+  
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
